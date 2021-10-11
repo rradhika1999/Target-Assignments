4948c2dee9a16992ae1dc6c90d82325a19b5ba27
rradhika1999 <39021888+rradhika1999@users.noreply.github.com>
2021-10-11 23:33:10 +0530

    Delete ProcessingAmount.java

--- Target_Assignments/ProcessingAmount.java
+++
@@ -1,8 +0,0 @@
-package Session7.ShoppingCart;
-
-public interface ProcessingAmount
-{
-    int ProcessTheAmountForFurniture(int a) throws Exception;
-    int ProcessTheAmountForGroceries(int b) throws Exception;
-    int ProcessTheAmountForElectronics(int c) throws Exception;
-}
