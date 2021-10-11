841c01b63323d96d2bbf64c7e48c662da95dfa3f
Ashwin Shenoi <30829930+ashwinshenoi99@users.noreply.github.com>
2021-10-11 23:19:23 +0530

    Update ArrayException.java
    Addition of comments to enhance readability.

--- Target_Assignments/src/ArrayException.java
+++ Target_Assignments/src/ArrayException.java
@@ -4,26 +4,31 @@ public class ArrayException extends Exception
 {
 	public static void main(String[] args)
 	{
+		// Scanner object creation
 		Scanner sc = new Scanner(System.in);
+		// Variable declaration
 		int position, size;
 		System.out.println("Enter the size of the array: ");
+		// Accepting input from the user dynamically
 		size = sc.nextInt();
-		int a[]=new int[size];
+		// Dynamic allocation of array
+		int a[] = new int[size];
 		System.out.println("Enter the elements in the arrray: ");
-		for(int i=0;i<size;i++)
+		for(int i=0; i<size; i++)
 		{
-			a[i]=sc.nextInt();
+			// Accepting input dynamically into the array
+			a[i] = sc.nextInt();
 		}
 		try
 		{
 			System.out.println("Enter the position: ");
 			position = sc.nextInt();
-			System.out.println("Element in the given position is: "+a[position-1]);
+			System.out.println("Element in the given position is: " + a[position-1]);
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
 			System.out.println("Enter a valid position");
-			System.out.println("Note: The position value should vie from 1 to "+size);
+			System.out.println("Note: The position value should vie from 1 to " + size);
 		}
 	}
 }
